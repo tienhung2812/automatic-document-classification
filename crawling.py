a579dc821c92351babbc5770f64d92d46b678c8a
Nguyen Tien Hung <hung.nguyentien97>
2018-12-17 23:20:12 +0700

    Change to reader view

--- crawling.py
+++ crawling.py
@@ -2,6 +2,7 @@ import urllib.request
 import time
 import pandas as pd
 from bs4 import BeautifulSoup
+from readability import Document
 import sys
 
 targetData = 5000
@@ -42,8 +43,8 @@ class NewsCrawler:
               shouldAdd = False
             if link['href'] in self.waitingList:
               shouldAdd = False
-            if self.findCate(link['href']) is None:
-              shouldAdd = False
+            # if self.findCate(link['href']) is None:
+            #   shouldAdd = False
             if shouldAdd:
               self.waitingList.append(link['href'])
           return result
@@ -56,27 +57,27 @@ class NewsCrawler:
           page_source = urllib.request.urlopen(url)
           self.crawledList.append(url)
           html = page_source.read().decode("utf8")
-          bsObj = BeautifulSoup(html,"html.parser")
+          doc = Document(html)
+          bsObj = BeautifulSoup(doc.summary(),"html.parser")
           
-          title = bsObj.find('h1', attrs={'class':'story-body__h1'}).text
-          content = bsObj.find('div', attrs={'class':'story-body__inner'})
+          title = str(doc.title)
+          # content = 
+          # bsObj.find('div', attrs={'class':'story-body__inner'})
           #Process Text
-          text = content.text
-          text = text.replace("(function() {","")
-          text = text.replace("if (window.bbcdotcom && bbcdotcom.adverts && bbcdotcom.adverts.slotAsync) {","")
-          text = text.replace("bbcdotcom.adverts.slotAsync(\'mpu\', [1,2,3]);","")
-#           text = text.replace("Search\n@-moz-keyframes gel-spin {\n0% {\n-moz-transform: rotate(0deg);\n100% {\n-moz-transform: rotate(360deg);@-webkit-keyframes gel-spin {\n0% {\n-webkit-transform: rotate(0deg);\n100% {\n-webkit-transform: rotate(360deg);@-ms-keyframes gel-spin {\n0% {\n-ms-transform: rotate(0deg);\n100% {\n-ms-transform: rotate(360deg);@keyframes gel-spin {\n0% {\ntransform: rotate(0deg);\n100% {\ntransform: rotate(360deg);.bbc-news-visual-journalism-loading-spinner {\ndisplay: block;\nmargin: 8px auto;\nwidth: 32px;\nheight: 32px;\nmax-width: 32px;\nfill: #323232;\n-webkit-animation-name: gel-spin;\n-webkit-animation-duration: 1s;\n-webkit-animation-iteration-count: infinite;\n-webkit-animation-timing-function: linear;\n-moz-animation-name: gel-spin;\n-moz-animation-duration: 1s;\n-moz-animation-iteration-count: infinite;\n-moz-animation-timing-function: linear;\nanimation-name: gel-spin;\nanimation-duration: 1s;\nanimation-iteration-count: infinite;\nanimation-timing-function: linear;html .bbc-news-vj-wrapper {font-family:Helmet,Freesans,Helvetica,Arial,sans-serif;\n(function(){\nvar initConfig = {\npathStatic: 'https://news.files.bbci.co.uk/include/newsspec/19077-brexit-glossary/assets/embed',\nprojectNamespace: 'newsspec-19077-brexit-glossary',\nversion: '1.1.14',\nlanguage: 'english',\nincludeName: 'main'\n;\nfunction cutsTheMustard() {\nreturn (\ndocument.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#BasicStructure', '1.1') &&\n'querySelector' in document &&\n'localStorage' in window &&\n'addEventListener' in window\n);if (cutsTheMustard()) {\nif (typeof require === 'undefined') {\nvar headTag = document.getElementsByTagName('head')[0],\nrequireTag = document.createElement('script');\nrequireTag.type = 'text/javascript';\nrequireTag.src = 'https://news.files.bbci.co.uk/include/newsspec/19077-brexit-glossary/assets/embed/js/lib/require/require-2.1.20.js';\nrequireTag.onload = initIframe;\nheadTag.appendChild(requireTag);\n else {\ninitIframe();\nfunction initIframe() {\nrequire(['https://news.files.bbci.co.uk/include/newsspec/19077-brexit-glossary/assets/embed/js/iframe-init.js'], function (initFullFatApplication) {\ninitFullFatApplication(initConfig);","")
-          
-          
-          text = text.replace("}","")
-          text = text.replace("})();","")
-          text = text.replace("/**/","")
-          text = text.replace(")();","")
+
+          text = str(bsObj.text)
+          text = text.replace("Image copyright","")
           text = text.replace("  ","")
           text = text.replace("\n\n","")
-          text = text.replace("Image copyright","")
-          text = text[:text.find('Search\n@-moz-keyframes gel-spin')]+text[text.rfind('initFullFatApplication(initConfig);\n);')+len('initFullFatApplication(initConfig);\n);'):]
-#           print(title+"\n\n"+text)
+          # text = text.replace("(function() {","")
+          # text = text.replace("if (window.bbcdotcom && bbcdotcom.adverts && bbcdotcom.adverts.slotAsync) {","")
+          # text = text.replace("bbcdotcom.adverts.slotAsync(\'mpu\', [1,2,3]);","")
+          # text = text.replace("}","")
+          # text = text.replace("})();","")
+          # text = text.replace("/**/","")
+          # text = text.replace(")();","")
+          # text = text[:text.find('Search\n@-moz-keyframes gel-spin')]+text[text.rfind('initFullFatApplication(initConfig);\n);')+len('initFullFatApplication(initConfig);\n);'):]
+          # print(title+"\n\n"+text)
           #Save text
           #Category
           if(cate is 'business'):
@@ -150,6 +151,9 @@ class NewsCrawler:
             self.getContent(link,cate)
             
             print("Crawling: \t"+str(i)+"/"+str(len(data))+"\tCate: "+cate+"\tSucess: "+str(self.sucess)+"\tUrl: "+str(link))
+          if self.sucess%10 == 0 and self.sucess>1:
+            print("Bu\tEn\tPo\tTe\tSp")
+            print(str(self.bu)+"\t"+str(self.en)+"\t"+str(self.po)+"\t"+str(self.te)+"\t"+str(self.sp))
           if(self.sucess>=targetData):
             self.ExportFile()
             
@@ -169,7 +173,7 @@ class NewsCrawler:
         self.filename =[]
         self.title = []
         self.content = []
-        self.waitingList = ["https://www.bbc.co.uk/"]
+        self.waitingList = ["https://www.bbc.co.uk/","http://www.bbc.com/","https://edition.cnn.com/"]
         self.crawledList = []
         en = "http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml"
         te = "http://feeds.bbci.co.uk/news/technology/rss.xml"
