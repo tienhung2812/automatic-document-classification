e4a5113abaf75af93cc72e737bc9eca7839f5a3c
Nguyen Tien Hung <hung.nguyentien97>
2018-12-18 20:47:31 +0700

    Commenting

--- ml.py
+++ ml.py
@@ -1,5 +1,6 @@
 import glob
 from pandas import DataFrame, read_csv
+import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import chi2
 from sklearn.manifold import TSNE
@@ -48,7 +49,7 @@ class ADC:
 
     def __init__(self):
         start = datetime.now()
-        
+        #DATASECTION
         import pandas as pd
         import glob
         print("Getting files list...")
@@ -125,126 +126,130 @@ class ADC:
         from sklearn.feature_selection import chi2
         import numpy as np
 
-        N = 3
+        N = 5
         for category, category_id in sorted(category_to_id.items()):
             features_chi2 = chi2(features, labels == category_id)
             indices = np.argsort(features_chi2[0])
             feature_names = np.array(self.tfidf.get_feature_names())[indices]
             unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
             bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
-            print("# '{}':".format(category))
-            print("  . Most correlated unigrams:\n       . {}".format('\n       . '.join(unigrams[-N:])))
-            print("  . Most correlated bigrams:\n       . {}".format('\n       . '.join(bigrams[-N:])))
+#             print("# '{}':".format(category))
+#             print("  . Most correlated unigrams:\n       . {}".format('\n       . '.join(unigrams[-N:])))
+#             print("  . Most correlated bigrams:\n       . {}".format('\n       . '.join(bigrams[-N:])))
 
 
         from sklearn.manifold import TSNE
-        import matplotlib.pyplot as plt
+        
         # Sampling a subset of our dataset because t-SNE is computationally expensive
         SAMPLE_SIZE = int(len(features) * 0.3)
         np.random.seed(0)
         indices = np.random.choice(range(len(features)), size=SAMPLE_SIZE, replace=False)
         projected_features = TSNE(n_components=2, random_state=0).fit_transform(features[indices])
-        colors = ['pink', 'green', 'midnightblue', 'orange', 'darkgrey']
-        for category, category_id in sorted(category_to_id.items()):
-            points = projected_features[(labels[indices] == category_id).values]
-            plt.scatter(points[:, 0], points[:, 1], s=30, c=colors[category_id], label=category)
-        plt.title("tf-idf feature vector for each article, projected on 2 dimensions.",
-                fontdict=dict(fontsize=15))
-        plt.legend()
+#         colors = ['pink', 'green', 'midnightblue', 'orange', 'darkgrey']
+#         for category, category_id in sorted(category_to_id.items()):
+#             points = projected_features[(labels[indices] == category_id).values]
+#             plt.scatter(points[:, 0], points[:, 1], s=30, c=colors[category_id], label=category)
+#         plt.title("tf-idf feature vector for each article, projected on 2 dimensions.",
+#                 fontdict=dict(fontsize=15))
+#         plt.legend()
 
 
-        df[df.title.str.contains('Arsenal')]
-
+#         df[df.title.str.contains('Arsenal')]
+        #ALGORITHM SECTION
         dataexplorationtime = datetime.now()- createdatatime -start
-        
-
+        startAccuracyTime = datetime.now()
         from sklearn.linear_model import LogisticRegression
         from sklearn.ensemble import RandomForestClassifier
         from sklearn.neighbors import KNeighborsClassifier
+        from sklearn.svm import SVC
+#         from sklearn.naive_bayes import MultinomialNB
 
         from sklearn.model_selection import cross_val_score
-
-
+        import matplotlib.pyplot as plt
+        #COMPARE ACCURACY
         models = [
             RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0),
-            KNeighborsClassifier(n_neighbors=49),
-            LogisticRegression(random_state=0),
+#             SVC(gamma='scale')
+#             KNeighborsClassifier(n_neighbors=49)
+            LogisticRegression(random_state=0)
         ]
         CV = 5
         cv_df = pd.DataFrame(index=range(CV * len(models)))
         entries = []
         for model in models:
-            model_name = model.__class__.__name__
-            accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV)
-        for fold_idx, accuracy in enumerate(accuracies):
+          acctime = datetime.now()
+          model_name = model.__class__.__name__
+          print(model_name)
+          accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV)
+          for fold_idx, accuracy in enumerate(accuracies):
             entries.append((model_name, fold_idx, accuracy))
+          print("Time: "+str(datetime.now() - acctime))
         cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
-
-        import seaborn as sns
-        sns.boxplot(x='model_name', y='accuracy', data=cv_df)
-        sns.stripplot(x='model_name', y='accuracy', data=cv_df, 
-                    size=8, jitter=True, edgecolor="gray", linewidth=2)
-        print("Accuracy")
+        
+#         import seaborn as sns
+#         sns.boxplot(x='model_name', y='accuracy', data=cv_df)
+#         sns.stripplot(x='model_name', y='accuracy', data=cv_df,size=8, jitter=True, edgecolor="gray", linewidth=2)
+#         sns.plt.show()
+        
         print(cv_df.groupby('model_name').accuracy.mean())
-
-
-        # from sklearn.model_selection import train_test_split
-
-        # self.model = LogisticRegression(random_state=0)
-
-        # X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
-        # self.model.fit(X_train, y_train)
-        # y_pred_proba = self.model.predict_proba(X_test)
-        # y_pred = self.model.predict(X_test)
-
-        # from sklearn.metrics import confusion_matrix
-        # import seaborn as sns
-
-        # conf_mat = confusion_matrix(y_test, y_pred)
-        # sns.heatmap(conf_mat, annot=True, fmt='d',
-        #             xticklabels=category_id_df.category.values, yticklabels=category_id_df.category.values)
-        # plt.ylabel('Actual')
-        # plt.xlabel('Predicted')
-
-        # from IPython.display import display
-
-        # for predicted in category_id_df.category_id:
-        #     for actual in category_id_df.category_id:
-        #         if predicted != actual and conf_mat[actual, predicted] >= 2:
-        #             print("'{}' predicted as '{}' : {} examples.".format(self.id_to_category[actual], self.id_to_category[predicted], conf_mat[actual, predicted]))
-        #             display(df.loc[indices_test[(y_test == actual) & (y_pred == predicted)]][['title', 'content']])
-        #             print('')
-
+        print("Accuracy Counting Time:"+str(datetime.now() - startAccuracyTime))
+        #KNN
+#         from sklearn.model_selection import train_test_split
         
+#         self.KNNmodel = KNeighborsClassifier(n_neighbors=49)
 
-        # self.model.fit(features, labels)
-
-        # from sklearn.feature_selection import chi2
-
-        # N = 5
-        # for category, category_id in sorted(category_to_id.items()):
-        #     indices = np.argsort(self.model.coef_[category_id])
-        #     feature_names = np.array(self.tfidf.get_feature_names())[indices]
-        #     unigrams = [v for v in reversed(feature_names) if len(v.split(' ')) == 1][:N]
-        #     bigrams = [v for v in reversed(feature_names) if len(v.split(' ')) == 2][:N]
-        #     print("# '{}':".format(category))
-        #     print("  . Top unigrams:\n       . {}".format('\n       . '.join(unigrams)))
-        #     print("  . Top bigrams:\n       . {}".format('\n       . '.join(bigrams)))
-
+#         X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
+#         self.KNNmodel.fit(X_train, y_train)
+#         y_pred_proba = self.KNNmodel.predict_proba(X_test)
+#         y_pred = self.KNNmodel.predict(X_test)
+#         self.KNNmodel.fit(features, labels)
         
-        # trainingtime = datetime.now()-createdatatime-dataexplorationtime-start
-        # df[df.content.str.lower().str.contains('news website')].category.value_counts()
+        #Random Forest
+        rftime = datetime.now()
+        from sklearn.model_selection import train_test_split
+        
+        self.RFmodel = RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0)
 
+        X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
+        self.RFmodel.fit(X_train, y_train)
+        y_pred_proba = self.RFmodel.predict_proba(X_test)
+        y_pred = self.RFmodel.predict(X_test)
+        self.RFmodel.fit(features, labels)
+        print("Training Random Forest Time:"+str(datetime.now() - rftime))
+        
+        
+#       LogisticRegression
+        LRtime = datetime.now()
+
+        from sklearn.model_selection import train_test_split
+        
+        self.LRmodel = LogisticRegression(random_state=0)
+
+        X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
+        self.LRmodel.fit(X_train, y_train)
+        y_pred_proba = self.LRmodel.predict_proba(X_test)
+        y_pred = self.LRmodel.predict(X_test)
+        self.LRmodel.fit(features, labels)
+        
+        trainingtime = datetime.now()-createdatatime-dataexplorationtime-start
+        df[df.content.str.lower().str.contains('news website')].category.value_counts()
+        print("Training LogisticRegression Time:"+str(datetime.now() - LRtime))
+        
         
     def Predict(self,raw_text):
         given_text=[raw_text]
         text_features = self.tfidf.transform(given_text)
-        predictions = self.model.predict(text_features)
-        for text, predicted in zip(given_text, predictions):
-            # print('"{}"'.format(text))
-            # print("  - Predicted as: '{}'".format(self.id_to_category[predicted]))
-            # print("")
+
+        RFpredictions = self.RFmodel.predict(text_features)
+        for text, predicted in zip(given_text, RFpredictions):
+            print("Random Forest")
+            print('"{}"'.format(text))
+            print("  - Predicted as: '{}'".format(self.id_to_category[predicted]))
+            print("")
+        LRpredictions = self.LRmodel.predict(text_features)
+        for text, predicted in zip(given_text, LRpredictions):
+            print("LogisticRegression")
+            print('"{}"'.format(text))
+            print("  - Predicted as: '{}'".format(self.id_to_category[predicted]))
+            print("")
             return self.id_to_category[predicted]
-adc = ADC()
-print(adc.Predict("sffs"))
-    
